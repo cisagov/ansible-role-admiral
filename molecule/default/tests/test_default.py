bcca47c1b0bf1c1101babfb1abe8b2b30f752f75
Alexander King <alexander.king@cisa.dhs.gov>
2023-04-17 13:46:02 -0500

    Update the default test scenario
    We verify that the correct directory exists with the correct files and
    permisions.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -12,7 +12,23 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts("all")
 
 
-@pytest.mark.parametrize("x", [True])
-def test_packages(host, x):
-    """Run a dummy test, just to show what one would look like."""
-    assert x
+@pytest.mark.parametrize("directory", [{"path": "/var/cyhy/admiral", "mode": "0o755"}])
+def test_packages(host, directory):
+    """Assert that the correct directory was created."""
+    assert host.file(directory["path"]).exists
+    assert host.file(directory["path"]).is_directory
+    assert oct(host.file(directory["path"]).mode) == directory["mode"]
+
+
+@pytest.mark.parametrize(
+    "file",
+    [
+        {"path": "/var/cyhy/admiral/docker-compose.yml", "mode": "0o644"},
+        {"path": "/var/cyhy/admiral/docker-compose-dev.yml", "mode": "0o644"},
+    ],
+)
+def test_command(host, file):
+    """Assert that the correct files exist."""
+    assert host.file(file["path"]).exists
+    assert host.file(file["path"]).is_file
+    assert oct(host.file(file["path"]).mode) == file["mode"]
